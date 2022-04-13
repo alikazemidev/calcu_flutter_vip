1e622d8e7b649c119605fea476d73e1471651e4f
ali kazemi <alikazemidev@gmail.comi>
2022-04-13 12:15:18 +0430

    complete add just one bug left (multiple operation add) to fix

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 final Color textGreen = Color(0xff38b387);
 final Color textGrey = Color(0xff93a59e);
 final Color backgroundGrey = Color(0xff2e3951);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calcu_vip_flutter/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +18,147 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String outPut = '';
+  var _result = '';
+
+// *get operator
+  bool isOperator(String text) {
+    var opeatorList = ['ac', 'ce', '%', '/', '=', '+', '-', '*'];
+    for (var item in opeatorList) {
+      if (text == item) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void _showOutPut(String text) {
+    setState(() {
+      outPut = outPut + text;
+    });
+  }
+
+// *row item
+  Widget _rowItem(String text1, String text2, String text3, String text4) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(
+          style: isOperator(text1)
+              ? TextButton.styleFrom(
+                  primary: textGreen,
+                  backgroundColor: backgroundGreyDark,
+                  shape: CircleBorder(),
+                )
+              : null,
+          onPressed: () {
+            if (text1 == 'ac') {
+              setState(() {
+                _result = '';
+                outPut = '';
+              });
+            } else {
+              _showOutPut(text1);
+            }
+          },
+          child: Text(
+            text1,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 26,
+              color: isOperator(text1) ? textGreen : textGrey,
+            ),
+          ),
+        ),
+        TextButton(
+          style: isOperator(text2)
+              ? TextButton.styleFrom(
+                  primary: textGreen,
+                  backgroundColor: backgroundGreyDark,
+                  shape: CircleBorder(),
+                )
+              : null,
+          onPressed: () {
+            if (text2 == 'ce') {
+              setState(() {
+                if (outPut.length > 0) {
+                  outPut = outPut.substring(0, outPut.length - 1);
+                }
+              });
+            } else {
+              _showOutPut(text2);
+            }
+          },
+          child: Text(
+            text2,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 26,
+              color: isOperator(text2) ? textGreen : textGrey,
+            ),
+          ),
+        ),
+        TextButton(
+          style: isOperator(text3)
+              ? TextButton.styleFrom(
+                  backgroundColor: backgroundGreyDark,
+                  shape: CircleBorder(),
+                )
+              : null,
+          onPressed: () {
+            _showOutPut(text3);
+          },
+          child: Text(
+            text3,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 26,
+              color: isOperator(text3) ? textGreen : textGrey,
+            ),
+          ),
+        ),
+        TextButton(
+          style: isOperator(text4)
+              ? TextButton.styleFrom(
+                  primary: textGreen,
+                  backgroundColor: backgroundGreyDark,
+                  shape: CircleBorder(),
+                )
+              : null,
+          onPressed: () {
+            if (text4 == '=') {
+              if (!_result.isEmpty) {
+                return;
+              }
+              var parser = Parser();
+              Expression expression = parser.parse(outPut);
+              ContextModel contextModel = ContextModel();
+              var result =
+                  expression.evaluate(EvaluationType.REAL, contextModel);
+              setState(() {
+                _result = result.toString();
+              });
+            }
+            _showOutPut(text4);
+          },
+          child: Text(
+            text4,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 26,
+              color: isOperator(text4) ? textGreen : textGrey,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +170,35 @@ class HomePage extends StatelessWidget {
               flex: 3,
               child: Container(
                 color: backgroundGreyDark,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        outPut,
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          color: textGreen,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _result,
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 65,
+                          color: textGrey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
@@ -39,122 +208,17 @@ class HomePage extends StatelessWidget {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      RowItem('ac', 'ce', '%', '/'),
-                      RowItem('7', '8', '9', '*'),
-                      RowItem('4', '5', '6', '-'),
-                      RowItem('1', '2', '3', '+'),
-                      RowItem('00', '0', '.', '='),
+                      _rowItem('ac', 'ce', '%', '/'),
+                      _rowItem('7', '8', '9', '*'),
+                      _rowItem('4', '5', '6', '-'),
+                      _rowItem('1', '2', '3', '+'),
+                      _rowItem('00', '0', '.', '='),
                     ],
                   )),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-// *row item
-class RowItem extends StatelessWidget {
-  final String? text1;
-  final String? text2;
-  final String? text3;
-  final String? text4;
-  RowItem(
-    this.text1,
-    this.text2,
-    this.text3,
-    this.text4,
-  );
-
-  bool isOperator(String text) {
-    var opeatorList = ['ac', 'ce', '%', '/', '=', '+', '-', '*'];
-    for (var item in opeatorList) {
-      if (text == item) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        TextButton(
-          style: isOperator(text1!)
-              ? TextButton.styleFrom(
-                  primary: textGreen,
-                  backgroundColor: backgroundGreyDark,
-                  shape: CircleBorder(),
-                )
-              : null,
-          onPressed: () {},
-          child: Text(
-            text1!,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 26,
-              color: textGrey,
-            ),
-          ),
-        ),
-        TextButton(
-          style: isOperator(text2!)
-              ? TextButton.styleFrom(
-                  primary: textGreen,
-                  backgroundColor: backgroundGreyDark,
-                  shape: CircleBorder(),
-                )
-              : null,
-          onPressed: () {},
-          child: Text(
-            text2!,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 26,
-              color: textGrey,
-            ),
-          ),
-        ),
-        TextButton(
-          style: isOperator(text3!)
-              ? TextButton.styleFrom(
-                  primary: textGreen,
-                  backgroundColor: backgroundGreyDark,
-                  shape: CircleBorder(),
-                )
-              : null,
-          onPressed: () {},
-          child: Text(
-            text3!,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 26,
-              color: textGrey,
-            ),
-          ),
-        ),
-        TextButton(
-          style: isOperator(text4!)
-              ? TextButton.styleFrom(
-                  primary: textGreen,
-                  backgroundColor: backgroundGreyDark,
-                  shape: CircleBorder(),
-                )
-              : null,
-          onPressed: () {},
-          child: Text(
-            text4!,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 26,
-              color: textGrey,
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
